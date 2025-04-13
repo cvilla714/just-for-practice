c40b195056b7dc7a6f1e6f4de6df5adef1cd895e
Cosmel VG <cosmelvg@gmail.com>
2025-04-13 22:38:16 +0000

    Add useEffect and to fetch data Add div to map list all post

--- my-project/src/App.tsx
+++ my-project/src/App.tsx
@@ -1,10 +1,33 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { fecthPosts } from './utils/fetchPost'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [loading , setLoading] = useState(true);
+  const [posts , setPosts] = useState<Post[]>([]);
+  const [error , setError] = useState<string | null>(null); 
+
+  useEffect(() => {
+    const loadData = async () =>{
+      try{
+        const data = await fecthPosts();
+        setPosts(data);
+      }catch(err){
+        setError(err instanceof Error ? err.message: 'Failed to load posts');
+      }finally{
+        setLoading(false);
+      }
+    };
+    loadData();
+  },[]);
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error}</div>;
+
+
 
   return (
     <>
@@ -28,6 +51,15 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <div>
+      <h1>Posts</h1>
+      {posts.map((post) => (
+        <article key={post.id}>
+          <h2>{post.title}</h2>
+          <p>{post.body}</p>
+        </article>
+      ))}
+    </div>
     </>
   )
 }
