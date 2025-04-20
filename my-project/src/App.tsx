e356475265f31be09c91d89355577b0520d6b477
Cosmel VG <cosmelvg@gmail.com>
2025-04-20 04:15:28 +0000

    Include new users component

--- my-project/src/App.tsx
+++ my-project/src/App.tsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { fecthPosts } from './utils/fetchPost'
+import Users from './components/Users'
 
 function App() {
   const [count, setCount] = useState(0);
@@ -74,6 +75,7 @@ function App() {
         </article>
       ))}
     </div>
+    <Users/>
     </>
   )
 }
